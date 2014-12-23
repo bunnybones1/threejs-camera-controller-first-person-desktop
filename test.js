ea634eee8927474eba0ac1881743d103c4dc638b
Tomasz Dysinski <oz@bunnybones.com>
2014-12-23 18:12:45 -0500

    fixed dep

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ var onReady = function() {
 	var scene = view.scene;
 
 	var sphereGeometry = new THREE.SphereGeometry(1.5);
-	var size = 100;
+	var size = 500;
 	var sizeHalf = size * .5;
 	var bounds = new THREE.Box3(
 		new THREE.Vector3(-sizeHalf, -sizeHalf, -sizeHalf),
@@ -15,7 +15,7 @@ var onReady = function() {
 	)
 	var random = new THREE.Vector3();
 	var boundSize = bounds.size();
-	for (var i = 0; i < 600; i++) {
+	for (var i = 0; i < 1200; i++) {
 		var ball = new THREE.Mesh(sphereGeometry);
 		scene.add(ball);
 		random.set(
