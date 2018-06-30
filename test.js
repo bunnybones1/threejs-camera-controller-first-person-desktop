6358510cba5270c52d2ae1268ba88e59cb2300dc
Tomasz Dysinski <tom@wgames.com>
2018-06-30 10:59:38 -0400

    upgraded threejs and tested

--- test.js
+++ test.js
@@ -1,49 +1,40 @@
-var onReady = function() {
-	var View = require('threejs-managed-view').View;
-	var CameraControllerFPS = require('./');
-	var view = new View({
-		useRafPolyfill: false
-	});
-	var scene = view.scene;
+var three = require('three');
+var View = require('threejs-managed-view').View;
+var CameraControllerFPS = require('./');
+var view = new View({
+	useRafPolyfill: false
+});
+var scene = view.scene;
 
-	var sphereGeometry = new THREE.SphereGeometry(1.5);
-	var size = 500;
-	var sizeHalf = size * .5;
-	var bounds = new THREE.Box3(
-		new THREE.Vector3(-sizeHalf, -sizeHalf, -sizeHalf),
-		new THREE.Vector3(sizeHalf, sizeHalf, sizeHalf)
-	)
-	var random = new THREE.Vector3();
-	var boundSize = bounds.size();
-	for (var i = 0; i < 1200; i++) {
-		var ball = new THREE.Mesh(sphereGeometry);
-		scene.add(ball);
-		random.set(
-			Math.random(),
-			Math.random(),
-			Math.random()
-		);
-		ball.position.copy(bounds.min).add(random.multiply(boundSize));
-	};
+var sphereGeometry = new three.SphereGeometry(1.5);
+var size = 500;
+var sizeHalf = size * .5;
+var bounds = new three.Box3(
+	new three.Vector3(-sizeHalf, -sizeHalf, -sizeHalf),
+	new three.Vector3(sizeHalf, sizeHalf, sizeHalf)
+)
+var random = new three.Vector3();
+var boundSize = bounds.size();
+for (var i = 0; i < 1200; i++) {
+	var ball = new three.Mesh(sphereGeometry);
+	scene.add(ball);
+	random.set(
+		Math.random(),
+		Math.random(),
+		Math.random()
+	);
+	ball.position.copy(bounds.min).add(random.multiply(boundSize));
+};
 
-	var fpsController = new CameraControllerFPS(view.camera, view.canvasContainer);
+var fpsController = new CameraControllerFPS(view.camera, view.canvasContainer);
 
-	fpsController.onPointerLockAttainSignal.add(function(){
-		console.log('attained pointerlock');
-	})
-	fpsController.onPointerLockReleaseSignal.add(function(){
-		console.log('released pointerlock');
-	})
+fpsController.onPointerLockAttainSignal.add(function(){
+	console.log('attained pointerlock');
+})
+fpsController.onPointerLockReleaseSignal.add(function(){
+	console.log('released pointerlock');
+})
 
-	view.renderManager.onEnterFrame.add(function() {
-		fpsController.update();
-	})
-}
-
-var loadAndRunScripts = require('loadandrunscripts');
-loadAndRunScripts(
-	[
-		'bower_components/three.js/three.js'
-	],
-	onReady
-);
+view.renderManager.onEnterFrame.add(function() {
+	fpsController.update();
+})
